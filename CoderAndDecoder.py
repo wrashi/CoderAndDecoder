32a4af892303906890c861865464156621cf8e18
wrashi <wrashi@mac.com>
2020-05-29 20:24:29 -0600

    One-Time Pad Implementation
    This code uses both upper and lower case letters to create a different encryption pad every time the program is run.

--- CoderAndDecoder.py
+++ CoderAndDecoder.py
@@ -1,95 +1,74 @@
-#encriptanddecript
-from tkinter import messagebox, simpledialog, Tk
-root = Tk()
+import tkinter as tk
+from tkinter import ttk, messagebox, simpledialog
+import string, random
 
-Encripting_Codes = {
-    "a": "8",
-    'b': ",",
-    "c": ".",
-    "d": "~",
-    "e": "2",
-    "f": "{",
-    "g": "}",
-    "h": "=",
-    'i': "_",
-    'j': "+",
-    'k': "-",
-    'l': "(",
-    'm': ")",
-    'n': "#",
-    'o': "6",
-    'p': "@",
-    'q': "*",
-    'r': "^",
-    's': ";",
-    't': ":",
-    'u': "1",
-    'v': "<",
-    'w': ">",
-    'x': "/",
-    'y': "&",
-    'z': '!',
-    ' ': '  ',
-}
-Decripting_codes = {
-    "8": "a",
-    ",": "b",
-    ".": "c",
-    "~": "d",
-    "2": "e",
-    "}": "f",
-    "{": "g",
-    "=": "h",
-    "_": "i",
-    "+": "j",
-    "-": "k",
-    ")": "l",
-    "(": "m",
-    "#": "n",
-    "6": "o",
-    "@": "p",
-    "*": "q",
-    "^": "r",
-    ";": "s",
-    ":": "t",
-    "1": "u",
-    "<": "v",
-    ">": "w",
-    "/": "x",
-    "&": "y",
-    "!": "z",
-    ' ': "  ",
-}
+#	Test Case 1: 
+#		Input:	the quick brown fox jumped over the lazy dog
+#		Output:	:=2 *1_.- ,^6># {6/ +1)@2~ 6<2^ :=2 (8!& ~6}
 
-#decript
-def decript (lets_go):
-    Message = simpledialog.askstring('Message', 'What is your message')
-    Part = (" ")
-    for  letters in (Message):
-        Final_Encription = (Decripting_codes.get(letters))
-        Final_Encription = (Part + Final_Encription)
-        Part = Final_Encription
-    messagebox.showinfo('Your decoded message is',  Part)
+#	Global Variables
 
-#encript
-def encript (lets_go):
-    Message = simpledialog.askstring('Message', 'What is your message')
-    Part = (" ")
-    for letter in (Message):
-        Final_Encription = (Encripting_Codes.get(letter))
-        Final_Encription = (Part + Final_Encription)
-        Part = Final_Encription
-    messagebox.showinfo('Your coded message is',  Part)
+Encrypting_Codes = {}
+characters = list(string.ascii_uppercase) + list(string.ascii_lowercase)
+shuffled = characters.copy()
+random.shuffle(shuffled)
 
-#AskUserWhatTheyWantToDo
-What_to_do = simpledialog.askstring('What do you want to do', 'What would you like to do')
+for index, value in enumerate(characters):
+	Encrypting_Codes[characters[index]] = shuffled[index]
+print(Encrypting_Codes)
 
-def do (command):
-    if command == 'code':
-        encript ('encripting')
-    elif command == 'decode':
-        decript ('decipting')
-    else:
-        messagebox.showinfo('Error', 'Error')
+Decrypting_codes = {}
+for key, value in Encrypting_Codes.items():
+	Decrypting_codes[value] = key
 
-do (What_to_do)
+#	Functions
+
+# Function for when the combobox item is chosen
+def callbackFunc(event):
+	command = comboChoice.get()
+	if command == 'Code':
+		crypt ('encrypting')
+	elif command == 'Decode':
+		crypt ('decrypting')
+		
+#	encryput / decrypt a message
+def crypt (lets_go):
+	Message = simpledialog.askstring('Message', 'What is your message')
+	Part = ("")
+	for	 letter in (Message):
+		if lets_go == 'encrypting':
+			Final_Encryption = (Encrypting_Codes.get(letter))
+		else:
+			Final_Encryption = (Decrypting_codes.get(letter))
+		if Final_Encryption == None:
+			print("I don't know '{}'. Passing through…".format(letter))
+			Part += letter
+		else:
+			Part += Final_Encryption
+	message = "Your message is\n'{}'".format(Part)
+	codeMessage.set(message)
+	print(message)
+
+#	Gui Code
+
+app = tk.Tk() 
+WINDOWWIDTH = 300
+app.geometry('{}x150'.format(WINDOWWIDTH))
+
+#	Label
+labelTop = tk.Label(app, text = "What do you want to do?")
+labelTop.grid(column=0, row=0)
+
+#	Combobox
+comboChoice = ttk.Combobox(app, values=[
+	"Code", 
+	"Decode"])
+comboChoice.grid(column=0, row=1)
+comboChoice.bind("<<ComboboxSelected>>", callbackFunc) # Calls the function on selection
+
+#	Message
+codeMessage = tk.StringVar()
+labelBottom = tk.Label(app, textvariable=codeMessage, wraplength=WINDOWWIDTH-10)
+labelBottom.grid(column=0,row=2)
+
+app.mainloop()
